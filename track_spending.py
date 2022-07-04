ec2e5c8bedf1d4748972b4100edc993324a099aa
David Wei <bigwei0425@gmail.com>
2022-07-04 15:56:41 +0800

    add header

--- track_spending.py
+++ track_spending.py
@@ -16,6 +16,7 @@ for p in track:
 	print(p[0], '的價格為', p[1])
 
 #寫入檔案
-with open('track_spending.csv', 'w') as t:
+with open('track_spending.csv', 'w', encoding='utf-8') as t:
+	t.write('商品,價格\n')
 	for p in track:
 		t.write(p[0] + ',' + str(p[1]) + '\n')
