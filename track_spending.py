ce70077af12d81214c01c249baf3c015b4d73e71
David Wei <bigwei0425@gmail.com>
2022-07-04 16:30:09 +0800

    final

--- track_spending.py
+++ track_spending.py
@@ -1,6 +1,16 @@
 #list中的list__二維清單
 
+#讀取檔案 : 叫系統去讀取track_spending.csv檔案,然後把它寫入到track的列表中
 track = []
+with open('track_spending.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line: 
+			continue  #當遇到資料為'商品,價格'時,跳到下一回,不繼續執行。continue可避免抓到不要的資料
+		name, price = line.strip().split(',') #刪除前後多於空格及換行後,用逗號將資料分開,已知資料會分為2塊,則直接定義給name及price
+		track.append([name, price])
+print(track)
+
+#讓使用者輸入
 while True:
 	product = input('請輸入商品名稱 : ')
 	if product == 'q':
@@ -11,7 +21,7 @@ print(track)
 
 #取二維清單中的值
 print(track[0][1])
-#也可用for將值取出
+#也可用for將值取出,印出所有購買紀錄
 for p in track:
 	print(p[0], '的價格為', p[1])
 
